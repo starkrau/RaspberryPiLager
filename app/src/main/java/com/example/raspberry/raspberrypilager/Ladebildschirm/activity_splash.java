224eb88a56b2f61a259fc6518d6fd3b11550dabc
starkrau <Arthur.Krauter@gmx.de>
2017-04-28 13:51:05 +0200

    Ladebildschirm wurde eingefügt und wird bearbeitet

---
+++ app/src/main/java/com/example/raspberry/raspberrypilager/Ladebildschirm/activity_splash.java
@@ -0,0 +1,16 @@
+package com.example.raspberry.raspberrypilager.Ladebildschirm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.raspberry.raspberrypilager.R;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
