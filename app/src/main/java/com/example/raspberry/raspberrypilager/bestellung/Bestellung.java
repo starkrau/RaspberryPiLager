67106106f76c9003ae9086a21008d5787abcad78
Stanislav <marchenkostas@gmx.de>
2017-04-30 18:37:04 +0200

    Der Name der Klasse sollte mit dem Grossbuchstabe beginnen.

--- app/src/main/java/com/example/raspberry/raspberrypilager/bestellung/Bestellung.java
+++ app/src/main/java/com/example/raspberry/raspberrypilager/bestellung/Bestellung.java
@@ -15,25 +15,26 @@ public class Bestellung extends AppCompatActivity {
 
     public static String status;
 
-    public void clickBestellen(View view){
+    public void clickBestellen(View view) {
 
         RadioButton sl1 = (RadioButton) findViewById(R.id.rb1);
         RadioButton sl2 = (RadioButton) findViewById(R.id.rb2);
         RadioButton sl3 = (RadioButton) findViewById(R.id.rb3);
 
-        if (sl1.isChecked()){
+        if (sl1.isChecked()) {
             status = "sl1";
-        } else if (sl2.isChecked()){
+        } else if (sl2.isChecked()) {
             status = "sl2";
-        } else if (sl3.isChecked()){
+        } else if (sl3.isChecked()) {
             status = "sl3";
         } else {
             status = "Bitte was auswählen!";
             Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show();
             return;
-        };
+        }
+        ;
 
-       Intent i = new Intent(Bestellung.this,Bestaetigen.class);
+        Intent i = new Intent(Bestellung.this, Bestaetigen.class);
         i.putExtra("status", status);
         startActivity(i);
 
--- app/src/main/java/com/example/raspberry/raspberrypilager/menu/Menu.java
+++ app/src/main/java/com/example/raspberry/raspberrypilager/menu/Menu.java
@@ -7,22 +7,21 @@ import android.view.View;
 
 import com.example.raspberry.raspberrypilager.R;
 import com.example.raspberry.raspberrypilager.bestellung.Bestellung;
-import com.example.raspberry.raspberrypilager.uberuns.uberuns;
+import com.example.raspberry.raspberrypilager.uberuns.Uberuns;
 
 public class Menu extends AppCompatActivity {
 
-    public void clickBestellung(View view){
-        startActivity(new Intent(Menu.this,Bestellung.class));
+    public void clickBestellung(View view) {
+        startActivity(new Intent(Menu.this, Bestellung.class));
     }
 
-    public void clickUber(View view){
-        startActivity(new Intent(Menu.this,uberuns.class));
+    public void clickUber(View view) {
+        startActivity(new Intent(Menu.this, Uberuns.class));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        System.out.println("Bla-bla!");
     }
 }
