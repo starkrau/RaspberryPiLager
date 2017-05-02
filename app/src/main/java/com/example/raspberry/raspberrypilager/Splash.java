2da3b91a192117654be049886cf2e2cb69d8c4de
starkrau <Arthur.Krauter@gmx.de>
2017-05-02 13:45:32 +0200

    Ladebildschirm mit neuem Gif

---
+++ app/src/main/java/com/example/raspberry/raspberrypilager/Splash.java
@@ -0,0 +1,50 @@
+package com.example.raspberry.raspberrypilager;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.raspberry.raspberrypilager.menu.Menu;
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOExceptionWithCause;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Splash extends AppCompatActivity {
+
+    private GifImageView gifImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        gifImageView = (GifImageView) findViewById(R.id.gifImageView);
+
+        try {
+            InputStream inputStream = getAssets().open("gif.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Splash.this.startActivity(new Intent(Splash.this,Menu.class));
+                Splash.this.finish();
+            }
+        },3000);
+    }
+
+}
