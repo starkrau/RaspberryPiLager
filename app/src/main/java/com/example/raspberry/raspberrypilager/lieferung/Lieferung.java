8d4a7b1cab3e18534a2e21bc613708dbc755ba49
Yacine <onar651@gmail.com>
2017-05-22 14:51:53 +0200

    Ladebildschirm

--- app/src/main/java/com/example/raspberry/raspberrypilager/lieferung/Lieferung.java
+++ app/src/main/java/com/example/raspberry/raspberrypilager/lieferung/Lieferung.java
@@ -1,9 +1,21 @@
 package com.example.raspberry.raspberrypilager.lieferung;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.raspberry.raspberrypilager.R;
+import com.example.raspberry.raspberrypilager.Splash;
+import com.example.raspberry.raspberrypilager.menu.Menu;
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.example.raspberry.raspberrypilager.R.id.gifImageView;
 
 public class Lieferung extends AppCompatActivity {
 
@@ -11,5 +23,25 @@ public class Lieferung extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lieferung);
+
+        gifImageView2 = (GifImageView) findViewById(R.id.gifImageView2);
+
+        try {
+            InputStream inputStream = getAssets().open("post.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Lieferung.this.startActivity(new Intent(Lieferung.this,Menu.class));
+                Lieferung.this.finish();
+            }
+        },6000);
     }
 }
