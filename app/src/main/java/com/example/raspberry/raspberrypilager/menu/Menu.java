7225cc6f8c4e2426e5e976dbfc377c6636026547
starkrau <Arthur.Krauter@gmx.de>
2017-05-30 13:06:47 +0200

    Bugfixes vom Design Request an HttpServer

--- app/src/main/java/com/example/raspberry/raspberrypilager/menu/Menu.java
+++ app/src/main/java/com/example/raspberry/raspberrypilager/menu/Menu.java
@@ -4,6 +4,7 @@ import android.app.DownloadManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,20 +14,50 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.raspberry.raspberrypilager.R;
 import com.example.raspberry.raspberrypilager.bestellung.Bestellung;
 import com.example.raspberry.raspberrypilager.uberuns.Uberuns;
 
+import org.json.JSONObject;
+
 public class Menu extends AppCompatActivity {
 
-    String anh = "anh=100";
+    String anh = "anh=300";
 
     public void clickBestellung(View view) {
         startActivity(new Intent(Menu.this, Bestellung.class));
 
-        // Instantiate the RequestQueue.
+        String url = "https://dionysos.informatik.hs-augsburg.de/test/check.php?" + anh;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest check = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Toast.makeText(getApplicationContext(), "Response is: "+ response.toString(),Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            //    Toast.makeText(getApplicationContext(), "Response is: "+ error.toString(),Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), error.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(check);
+    }
+
+
+
+
+
+
+ /*       // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://dionysos.informatik.hs-augsburg.de/test/check.php?" + anh;
 
@@ -46,8 +77,8 @@ public class Menu extends AppCompatActivity {
         });
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
+*/
 
-    }
 
     public void clickUber(View view) {
 
